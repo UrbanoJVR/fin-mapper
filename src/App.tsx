715fee5c5bb7bfdb1371e08acbfccfe804262980
Urbano Villanueva <urbano.villanueva@autentia.com>
2023-11-28 06:25:33 +0100

    Router working but showing all pages instead navigate to

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,53 @@
 import React from 'react';
 import './App.css';
-import { Button, Typography, Card, CardContent } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {Button, Typography, Card, CardContent, Grid} from '@mui/material';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import CategoriesDashboard from './categories/categories-dashboard';
+import {Route, Routes, Link} from "react-router-dom";
 
-const darkTheme = createTheme({
+const theme = createTheme({
     palette: {
-        mode: 'dark',
+        mode: 'light',
     },
 });
 
 function App() {
-  return (
-      <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Card className="custom-card">
-              <CardContent>
-                  <Typography variant="h4" fontWeight="bold">
-                      FinMapper
-                  </Typography>
-                  <div className="button-container">
-                      <Button>
-                          Gestionar Categorías
-                      </Button>
-                      <Button>
-                          Cargar Archivo
-                      </Button>
-                  </div>
-              </CardContent>
-          </Card>
-      </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+
+            <Routes>
+                <Route path="cat1" element={<CategoriesDashboard/>}/>
+            </Routes>
+
+            <CssBaseline/>
+            <Card className="custom-card">
+                <CardContent>
+                    <Grid container spacing={2} direction="column">
+                        <Grid item>
+                            <Typography variant="h4" fontWeight="bold">
+                                FinMapper
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Link to="/cat1">
+                                Gestionar categorías
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Button>
+                                Cargar Archivo
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+            {/*<Switch>*/}
+            {/*    /!* Agrega una ruta para CategoriesDashboard *!/*/}
+            {/*    <Route path="/categories-dashboard" component={CategoriesDashboard} />*/}
+            {/*</Switch>*/}
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ src/categories/categories-dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CategoriesDashboard() {
+    return (
+        <h1> IT WORKS</h1>
+    );
+}
+
+export default CategoriesDashboard;
