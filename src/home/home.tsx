788ef6a1ec53e0bf053ab74eea2a48f04f35bd0b
Urbano Villanueva <urbano.villanueva@autentia.com>
2023-11-28 09:07:47 +0100

    Base home table

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,33 +1,63 @@
 import React from 'react';
 import './home.css';
-import {Button, Card, CardContent, Grid, Typography} from "@mui/material";
-import {Link} from "react-router-dom";
+import TableContainer from '@mui/material/TableContainer';
+import {Card, Paper, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
 import TopMenu from "../topMenu/app-bar";
 
 function Home() {
+    function createData(
+        name: string,
+        calories: number,
+        fat: number,
+        carbs: number,
+        protein: number,
+    ) {
+        return {name, calories, fat, carbs, protein};
+    }
+
+    const rows = [
+        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+        createData('Eclair', 262, 16.0, 24, 6.0),
+        createData('Cupcake', 305, 3.7, 67, 4.3),
+        createData('Gingerbread', 356, 16.0, 49, 3.9),
+    ];
+
     return (
         <>
             <TopMenu/>
-            <Card className="custom-card">
-                <CardContent>
-                    <Grid container spacing={2} direction="column">
-                        <Grid item>
-                            <Typography variant="h4" fontWeight="bold">
-                                FinMapper
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <Link to="/cat1">
-                                Gestionar categorías
-                            </Link>
-                        </Grid>
-                        <Grid item>
-                            <Button>
-                                Cargar Archivo
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </CardContent>
+            <Card sx={{margin: 10}}>
+                Últimos movimientos cargados:
+            </Card>
+            <Card sx={{margin: 10}}>
+                <TableContainer component={Paper}>
+                    <Table sx={{minWidth: 650}} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Fecha</TableCell>
+                                <TableCell align="right">Importe</TableCell>
+                                <TableCell align="right">Categoría</TableCell>
+                                <TableCell align="right">Concepto</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow
+                                    key={row.name}
+                                    sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.calories}</TableCell>
+                                    <TableCell align="right">{row.fat}</TableCell>
+                                    <TableCell align="right">{row.carbs}</TableCell>
+                                    <TableCell align="right">{row.protein}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </Card>
         </>
     );
